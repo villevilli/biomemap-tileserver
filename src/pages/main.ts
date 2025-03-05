9b62ec2c62c0c263c85aa75860f7d2ae4b8c2255
Ville Kujala <kujala.ville.vk@gmail.com>
2025-03-05 12:23:03 +0200

    display current mouse coordinates

--- src/pages/main.ts
+++ src/pages/main.ts
@@ -4,6 +4,27 @@ let map = leaflet.map('map', {
     crs: leaflet.CRS.Simple,
 }).setView([0.0, 0.0], 0);
 
+let MousePositionControl = leaflet.Control.extend({
+    _container: null,
+    options: {
+        position: 'bottomleft'
+    },
+
+    onAdd: function (map: leaflet.Map) {
+        var latlng = leaflet.DomUtil.create('div', 'mouseposition leaflet-control-attribution');
+        this._latlng = latlng;
+        return latlng;
+    },
+
+    updateHTML: function (latlng: leaflet.LatLng) {
+        this._latlng.innerHTML = `x: ${Math.round(latlng.lat)} z: ${Math.round(latlng.lng)}`;
+    }
+});
+
+let control = new MousePositionControl
+
+map.addControl(control)
+
 leaflet.tileLayer('http://localhost:3000/biomemap/{z}/{x}/{y}.png', {
     maxNativeZoom: 0,
     minNativeZoom: -8,
@@ -11,3 +32,7 @@ leaflet.tileLayer('http://localhost:3000/biomemap/{z}/{x}/{y}.png', {
     minZoom: -10,
     className: 'noblur'
 }).addTo(map);
+
+map.on("mousemove", (e) => {
+    control.updateHTML(e.latlng)
+})
