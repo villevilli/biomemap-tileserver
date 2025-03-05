377edbae03e1b5c1707289c09066d6460668d91e
Ville Kujala <kujala.ville.vk@gmail.com>
2025-03-05 11:29:25 +0200

    went back to serverside tile squashing

--- src/pages/main.ts
+++ src/pages/main.ts
@@ -6,35 +6,7 @@ let map = leaflet.map('map', {
 
 leaflet.tileLayer('http://localhost:3000/biomemap/{z}/{x}/{y}.png', {
     maxNativeZoom: 0,
-    minNativeZoom: 0,
-    maxZoom: 17,
-    minZoom: -1,
-}).addTo(map);
-
-leaflet.tileLayer('http://localhost:3000/biomemap/{z}/{x}/{y}.png', {
-    maxNativeZoom: -2,
-    minNativeZoom: -2,
-    maxZoom: -2,
-    minZoom: -3,
-}).addTo(map);
-
-leaflet.tileLayer('http://localhost:3000/biomemap/{z}/{x}/{y}.png', {
-    maxNativeZoom: -4,
-    minNativeZoom: -4,
-    maxZoom: -4,
-    minZoom: -5,
-}).addTo(map);
-
-leaflet.tileLayer('http://localhost:3000/biomemap/{z}/{x}/{y}.png', {
-    maxNativeZoom: -6,
-    minNativeZoom: -6,
-    maxZoom: -6,
-    minZoom: -7,
-}).addTo(map);
-
-leaflet.tileLayer('http://localhost:3000/biomemap/{z}/{x}/{y}.png', {
-    maxNativeZoom: -8,
     minNativeZoom: -8,
-    maxZoom: -8,
+    maxZoom: 17,
     minZoom: -10,
 }).addTo(map);
